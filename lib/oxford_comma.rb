c6578abebda360bd393b674900b556613ad46956
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 04:46:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,6 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   end
-  elsif array.length > 2
-    array.insert(-2, "and")
-    array.join(", ")
-  end
   elsif array.length < 2
     array.join
   end
