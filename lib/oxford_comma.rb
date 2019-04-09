057e81dbf7c68ccf587ea78b092dc37a2f1a208e
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 04:55:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 1
   	array.join
-  elsif array.length > 2
+  #elsif array.length > 2
   #if array.length > 2
     #return array[0..-2].join(", ") + ", and " + array[-1]
   #end
