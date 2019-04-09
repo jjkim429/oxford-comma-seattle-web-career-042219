673d421495377f9e5436c52ff4d0c39d94315c74
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 04:52:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   if array.length == 1
     return array
   end
-  if array.length > 2
-    return array[0..-2].join(", ") + ", and " + array[-1]
-  end
+  #if array.length > 2
+    #return array[0..-2].join(", ") + ", and " + array[-1]
+  #end
 end
